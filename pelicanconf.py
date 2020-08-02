a2b6a8ba8d24fee99e72762c8e832fe59dfa0b5f
reisub0 <reisub0@gmail.com>
2020-08-02 18:02:56 +0530

    Changes pagination to 10
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -93,7 +93,7 @@ SOCIAL = (
     ('rss', '/feeds/all.atom.xml'),
 )
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
