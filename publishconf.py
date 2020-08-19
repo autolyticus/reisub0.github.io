0ee3a854d92be04227139957e36c483f06f67c67
reisub0 <reisub0@gmail.com>
2020-08-19 22:17:03 +0530

    Updates analytics code
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- publishconf.py
+++ publishconf.py
@@ -22,4 +22,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = 'UA-131130629-1'
+GOOGLE_ANALYTICS = 'UA-131130629-2'
